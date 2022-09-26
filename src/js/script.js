2ae328411d472faca4a2e2a31aaa9f04bdb53112
Jonathan <jotaspequeno01@gmail.com>
2022-09-26 08:51:51 -0300

    troca de estilo btn

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,6 @@ function gerarLink() {
   let message = document.form_main.message.value;
   let url = 'https://wa.me/';
   let end_url = `${url}${number}?text=${message}`;
-  document.getElementById('end_url').innerText = ' ';
+  document.getElementById('end_url').innerText = ' Vamos para o Whatsapp? ';
   document.getElementById('end_url').href = end_url;
 }
