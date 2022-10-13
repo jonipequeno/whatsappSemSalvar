57a65858f97d583d4cb93058b32662e6ee5fe518
Jonathan Pequeno <jotaspequeno01@gmail.com>
2022-10-13 16:59:13 -0300

    Add codigo país com select

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
 /** @format */
 
 function gerarLink() {
-  
   let number = document.form_main.number.value;
   let message = document.form_main.message.value;
+  let pais = document.form_main.pais.value;
   let url = 'https://wa.me/';
-  let end_url = `${url}${number}?text=${message}`;
+  let end_url = `${url}${pais}${number}?text=${message}`;
   document.getElementById('end_url').innerText = ' Vamos para o Whatsapp? ';
   document.getElementById('end_url').href = end_url;
+ 
 }
